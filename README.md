53b316d20287965e23a8b84ef9dda08cba509712
andreast12 <andreast12.id@gmail.com>
2023-04-27 23:13:03 +0700

    add slider, scroll animation, social links, and more

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Web Development Bootcamp Landing Page
 
-Fake web development bootcamp landing page built with HTML, CSS, JS.
+Fake web development bootcamp landing page built with HTML, CSS, JS, and a few modules.
 
 The design is inspired from [this video](https://www.youtube.com/watch?v=4sosXZsdy-s).
--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+import sal from "sal.js";
+import { tns } from "./node_modules/tiny-slider/src/tiny-slider";
+
+// initialize sal.js
+sal();
+
+// initialize tiny-slider
+const testimonialsSlider = tns({
+  container: ".testimonials .slider",
+  items: 1,
+  slideBy: 1,
+  gutter: 30,
+  speed: 500,
+  loop: false,
+  rewind: true,
+  mouseDrag: true,
+  nav: false,
+  prevButton: ".testimonials .prev-btn",
+  nextButton: ".testimonials .next-btn",
+  responsive: {
+    769: {
+      items: 3,
+    },
+  },
+});
+
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navbarLinks = document.querySelector(".navbar-links");
 
